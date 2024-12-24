b5961de87f85b8c124b879eb9bb0b0cc09374d48
Aryan C <aryanc1027@gmail.com>
2024-12-23 19:13:57 -0500

    invisivle loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -3,9 +3,9 @@ import { Html } from '@react-three/drei';
 
 function Loader() {
   return (
-    <Html>
+    <Html center>
       <div className="flex justify-center items-center">
-        <div className="w-20 h-20 border-2 border-opacity-20 border-blue-500 rounded-full animate-spin" />
+        <div className="w-20 h-20 border-2 border-opacity-0 border-blue-500 rounded-full animate-spin" />
       </div>
     </Html>
   );
